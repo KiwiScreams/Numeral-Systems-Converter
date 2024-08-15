29fbbbc524e3fbbceeb148f71624d87b6156ea2d
KiwiScreams <screamskiwi3@gmail.com>
2024-08-15 19:18:09 +0400

    message panel

--- js/main.js
+++ js/main.js
@@ -5,20 +5,25 @@ const binaryButton = document.getElementById("binary-button");
 const octalButton = document.getElementById("octal-button");
 const decimalButton = document.getElementById("decimal-button");
 const hexadecimalButton = document.getElementById("hexadecimal-button");
+const fixedPanel = document.getElementById("fixed-panel");
+const panelText = document.getElementById("panel-text");
+const closeButton = document.getElementById("close-panel");
 
 function superFunc(base) {
   const number = numInput.value;
   const inputBase = parseInt(inputBaseSelect.value);
 
   if (!number) {
-    alert("Please enter a number.");
+    panelText.textContent = "Please enter a number";
+    fixedPanel.classList.add("show");
     return;
   }
 
   if (inputBase) {
     if (inputBase === 2) {
       if (!/^[01]+$/.test(number)) {
-        alert("Binary numbers can only contain 0 and 1.");
+        panelText.textContent = "Binary numbers can only contain 0 and 1";
+        fixedPanel.classList.add("show");
         return;
       }
     }
@@ -27,7 +32,8 @@ function superFunc(base) {
     const result = `${decimal.toString(base)}`;
     resultText.innerText = result;
   } else {
-    alert("Please select an input base.");
+    panelText.textContent = "Please select an input base";
+    fixedPanel.classList.add("show");
   }
 }
 
@@ -42,6 +48,11 @@ octalButton.addEventListener("click", function () {
 decimalButton.addEventListener("click", function () {
   superFunc(10);
 });
+
 hexadecimalButton.addEventListener("click", function () {
   superFunc(16);
+});
+
+closeButton.addEventListener("click", function () {
+  fixedPanel.classList.remove("show");
 });
