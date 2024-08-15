2509891f6264dddf6ac687706b4d689574f14f33
KiwiScreams <screamskiwi3@gmail.com>
2024-08-15 18:02:11 +0400

    system changing

--- js/main.js
+++ js/main.js
@@ -4,7 +4,25 @@ const resultDiv = document.getElementById("result-text");
 
 function superFunc(outputBase) {
   const inputBase = parseInt(inputBaseSelect.value, 10);
-  const num = parseInt(numInput.value, inputBase);
+  let num;
+  switch (inputBase) {
+    case 2:
+      num = binaryToDecimal(numInput.value);
+      break;
+    case 8:
+      num = octalToDecimal(numInput.value);
+      break;
+    case 10:
+      num = parseInt(numInput.value, 10);
+      break;
+    case 16:
+      num = hexadecimalToDecimal(numInput.value);
+      break;
+    default:
+      console.error("Invalid input base");
+      return;
+  }
+  const originalNum = numInput.value;
   const converted = num.toString(outputBase);
   resultDiv.innerText = `The ${
     outputBase === 2
@@ -14,5 +32,17 @@ function superFunc(outputBase) {
       : outputBase === 10
       ? "decimal"
       : "hexadecimal"
-  } representation of  ${converted}`;
+  } representation of ${originalNum} is ${converted}`;
+}
+
+function binaryToDecimal(binary) {
+  return parseInt(binary, 2);
+}
+
+function octalToDecimal(octal) {
+  return parseInt(octal, 8);
+}
+
+function hexadecimalToDecimal(hex) {
+  return parseInt(hex, 16);
 }
