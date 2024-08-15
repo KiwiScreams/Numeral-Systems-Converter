1587a710a8001f88cee0405b9d2bd076d04e2daf
KiwiScreams <screamskiwi3@gmail.com>
2024-08-15 18:12:52 +0400

    binary validation

--- js/main.js
+++ js/main.js
@@ -3,48 +3,25 @@ const inputBaseSelect = document.getElementById("input-base-select");
 const resultDiv = document.getElementById("result-text");
 
 function superFunc(base) {
-//   const inputBase = parseInt(inputBaseSelect.value, 10);
-//   let num;
-//   switch (inputBase) {
-//     case 2:
-//       num = binaryToDecimal(numInput.value);
-//       break;
-//     case 8:
-//       num = octalToDecimal(numInput.value);
-//       break;
-//     case 10:
-//       num = parseInt(numInput.value, 10);
-//       break;
-//     case 16:
-//       num = hexadecimalToDecimal(numInput.value);
-//       break;
-//     default:
-//       console.error("Invalid input base");
-//       return;
-//   }
-//   const originalNum = numInput.value;
-//   const converted = num.toString(outputBase);
-//   resultDiv.innerText = `The ${
-//     outputBase === 2
-//       ? "binary"
-//       : outputBase === 8
-//       ? "octal"
-//       : outputBase === 10
-//       ? "decimal"
-//       : "hexadecimal"
-//   } representation of ${originalNum} is ${converted}`;
-const numberInput = document.getElementById('number-input');
-    const inputBaseSelect = document.getElementById('input-base-select');
-    const resultText = document.getElementById('result-text');
+  const numberInput = document.getElementById('number-input');
+  const inputBaseSelect = document.getElementById('input-base-select');
+  const resultText = document.getElementById('result-text');
 
-    const number = numberInput.value;
-    const inputBase = parseInt(inputBaseSelect.value);
+  const number = numberInput.value;
+  const inputBase = parseInt(inputBaseSelect.value);
 
-    if (number && inputBase) {
-        const decimal = parseInt(number, inputBase);
-        const result = `Base ${base}: ${decimal.toString(base)}`;
-        resultText.innerText += result + '\n';
+  if (number && inputBase) {
+    if (inputBase === 2) { 
+      if (!/^[01]+$/.test(number)) {
+        alert("Binary numbers can only contain 0s and 1s.");
+        return;
+      }
     }
+
+    const decimal = parseInt(number, inputBase);
+    const result = `Base ${base}: ${decimal.toString(base)}`;
+    resultText.innerText += result + '\n';
+  }
 }
 
 function binaryToDecimal(binary) {
