0da20cab013949c7b64c3b411065c90623ded5ce
KiwiScreams <screamskiwi3@gmail.com>
2024-08-15 17:56:05 +0400

    multi system

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 const numInput = document.getElementById("number-input");
+const inputBaseSelect = document.getElementById("input-base-select");
 const resultDiv = document.getElementById("result-text");
-function superFunc(system) {
-  const num = parseInt(numInput.value, 10);
-  const binary = num.toString(system);
-  resultDiv.innerText = `The binary representation of ${num} is ${binary}`;
+
+function superFunc(outputBase) {
+  const inputBase = parseInt(inputBaseSelect.value, 10);
+  const num = parseInt(numInput.value, inputBase);
+  const converted = num.toString(outputBase);
+  resultDiv.innerText = `The ${
+    outputBase === 2
+      ? "binary"
+      : outputBase === 8
+      ? "octal"
+      : outputBase === 10
+      ? "decimal"
+      : "hexadecimal"
+  } representation of  ${converted}`;
 }
